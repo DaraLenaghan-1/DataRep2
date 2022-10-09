b07519d8a82d1ceb13e45a60e53bdf361242029e
Dara Lenaghan <61827277+DaraLenaghan-1@users.noreply.github.com>
2022-10-09 13:35:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,13 +22,13 @@ class App extends React.Component {
         <Container>
           <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>  <!-- This NavBar is visible on every page component of my application it contains a link to three other components called Home, Read & create. -->
+            <Nav.Link href="/">Home</Nav.Link>  /* This NavBar is visible on every page component of my application it contains a link to three other components called Home, Read & create. */
             <Nav.Link href="/read">Read</Nav.Link> 
             <Nav.Link href="/create">Create</Nav.Link>
           </Nav>
-        </Container>/*sacfdqwddqwdwq*/
+        </Container>
       </Navbar>
-      <Routes> <!-- Added routing to the application so that when you click on one of the links on the Navbar it will navigate to either the footer, header or content component -->
+      <Routes> /* Added routing to the application so that when you click on one of the links on the Navbar it will navigate to either the footer, header or content component */
       <Route path='/' element={<Content></Content>}></Route>
       <Route path='/read' element={<Header></Header>}></Route>
       <Route path='/create' element={<Footer></Footer>}></Route>
