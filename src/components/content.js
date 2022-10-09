ddfb50f37572962325f3d2a4dd4556e422337acd
Dara Lenaghan <61827277+DaraLenaghan-1@users.noreply.github.com>
2022-10-09 13:26:32 +0100

    Update content.js

--- src/components/content.js
+++ src/components/content.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export class Content extends React.Component{
+export class Content extends React.Component{ <!-- Made a folder under src/ called components and added a component to your application that will display the content as in part two. Your component logic should be
+added to a file called content. -->
     render(){
         return(
             <div>
             <h1>hello world</h1>
-             <h2>It is {new Date().toLocaleTimeString()}.</h2>
+             <h2>It is {new Date().toLocaleTimeString()}.</h2> <!-- dsiplays real time -->
             </div>
         )
     }
